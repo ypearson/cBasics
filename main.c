51f429b3e7948a6cdbb00b651cf7bc5efa9e3fe2
Yvan Pearson <ypearson@nklabs.com>
2016-10-10 23:33:12 -0400

    demos argc/argv

--- main.c
+++ main.c
@@ -5,10 +5,25 @@
 
 int main(int argc, char **argv)
 {
-	if(argc==1)
-	{
-        printf("%s\n","no args");
-	}
+    // argv is an array of pointers, each pointer dereferences to a pointer to a character
+
+    // argv + 0 points to the pointer that points to the 1st array (command name)
+    printf("  *(*(argv + 0) + 0) = %c", (char) *(*(argv + 0) + 0));
+    printf("  *(*(argv + 0) + 1) = %c", (char) *(*(argv + 0) + 1));
+    printf("  *(*(argv + 0) + 2) = %c", (char) *(*(argv + 0) + 2));
+    printf("  *(*(argv + 0) + 3) = %c", (char) *(*(argv + 0) + 3));
+    printf("  *(*(argv + 0) + 4) = %c", (char) *(*(argv + 0) + 4));
+    printf("\n");
+
+    // argv + 1 points to the pointer that points to the 2nd array (first arguement)
+    printf("  *(*(argv + 1) + 0) = %c", (char) *(*(argv + 1) + 0));
+    printf("  *(*(argv + 1) + 1) = %c", (char) *(*(argv + 1) + 1));
+    printf("  *(*(argv + 1) + 2) = %c", (char) *(*(argv + 1) + 2));
+    printf("  *(*(argv + 1) + 3) = %c", (char) *(*(argv + 1) + 3));
+    printf("  *(*(argv + 1) + 4) = %c", (char) *(*(argv + 1) + 4));
+    printf("\n");
+
+    printf("argc = %d", argc );
 
     fnc1(0);
     fnc2(0);
